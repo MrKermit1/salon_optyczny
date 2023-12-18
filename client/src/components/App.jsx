7474e6f5fa0ca8d2542ff0f9bfd8ecc535b0e3d8
MrKermit <olafisz.pl@gmail.com>
2023-12-18 21:17:33 +0100

    cos chcialem ale juz nie chce

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,15 +6,13 @@ import Home from './home/Home.jsx'
 import Account from './home/Account.jsx';
 export default function App() {
   
-
-
   return (
     <>
       <Routes>
           <Route path='/' element={<Main/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>}/>
-          <Route path='/home' element={<Home />}/>
+          <Route path='/home' element={<Home />} />
           <Route path='/acc' element={<Account />}/>
       </Routes>
     </>
