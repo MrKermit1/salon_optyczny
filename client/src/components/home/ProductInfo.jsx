932fc13395b4cdae0c78912f1ca161ffdba662c8
MrKermit <olafisz.pl@gmail.com>
2023-12-26 00:32:52 +0100

    strona z produktem

--- client/src/components/home/ProductInfo.jsx
+++ client/src/components/home/ProductInfo.jsx
@@ -3,14 +3,20 @@ import obrazek from '../../assets/okulary/obrazek1.jpg'
 function ProductInfo (props) {
 
     return (
-        <div className='bg-green-400 min-h-screen '>
-            <div className="ml-14 mt-14 float-left bg-green-400">
-                <img src={props.image != null ? props.image : obrazek} alt="" width="500"/>
+        <div className=' min-h-screen flex items-center flex-row justify-evenly'>
+            <div className=''>
+                <img src={props.image != null ? props.image : obrazek} alt="" width="500" className='rounded-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300'/>
             </div>
-            <div className='float-left bg-green-400 flex justify-center' >
-                <p>{props.opis}</p>
-                <button>Kup</button>
+            <div>
+                <p>Lorem ipsum. Dolor <br /><br /></p>
+                <p>Dolor Sit amet czy inne tam gówno po łacinie<br /><br /></p>
+                <p className='text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300'>
+                    <p className=' text-center'>120.99</p>
+                    <button className='text-center border-solid border-2 text-white bg-zinc-800 w-44 rounded-2xl '>Kup</button>
+                   
+                </p>
+                
             </div>
         </div>
     )
-}export default ProductInfo
+}export default ProductInfo 
